53f66cae8f9be32e26c4894afa0cc70183ab4d08
Luciano <lcnsqr@gmail.com>
2020-12-22 20:16:03 -0300

    Pontos na fronteira com libMesh::MeshTools::find_boundary_nodes()

--- libmesh/sandbox/mesh.cpp
+++ libmesh/sandbox/mesh.cpp
@@ -9,6 +9,7 @@
 #include "libmesh/libmesh.h"
 #include "libmesh/mesh.h"
 #include "libmesh/mesh_generation.h"
+#include "libmesh/elem.h"
 #include "libmesh/node.h"
 #include "libmesh/mesh_tools.h"
 #include "libmesh/boundary_info.h"
@@ -61,6 +62,7 @@ int main (int argc, char ** argv)
   // Print information about the mesh to the screen.
   mesh.print_info();
 
+	/*
   // Iterate through nodes
   libMesh::out << "Todos os nós da malha:" << std::endl;
   for (auto & node : mesh.local_node_ptr_range())
@@ -68,25 +70,53 @@ int main (int argc, char ** argv)
     libMesh::out << "ID: " << node->id() << ": ";
     libMesh::out << node->slice(0) << " " << node->slice(1) << " " << node->slice(2) << std::endl;
   }
+	*/
 
   // Boundary nodes
-  /*
   libMesh::out << "Nós na fronteira:" << std::endl;
   std::unordered_set<dof_id_type> block_boundary_nodes;
   block_boundary_nodes = libMesh::MeshTools::find_boundary_nodes(mesh);
   for (auto & id : block_boundary_nodes)
   {
     libMesh::out << "ID: " << id << std::endl;
+
+		Point p = mesh.node_ref(id);
+		libMesh::out << p(0) << ", " << p(1) << ", " << p(2) << std::endl;
   }
-  */
 
-  const BoundaryInfo & binfo = mesh.get_boundary_info();
-  binfo.print_summary();
+	/*
+  const BoundaryInfo & boundary_info = mesh.get_boundary_info();
+  boundary_info.print_summary();
 
-  for (auto & id : binfo.get_boundary_ids ())
+  for (auto & id : boundary_info.get_boundary_ids ())
   {
-    libMesh::out << "ID: " << id << ", nome: " << binfo.get_sideset_name(id) << std::endl;
+    libMesh::out << "ID: " << id << ", nome: " << boundary_info.get_sideset_name(id) << std::endl;
   }
+
+  // Element iterator
+  MeshBase::const_element_iterator el = mesh.active_elements_begin(), end_el = mesh.active_elements_end();
+  for ( ; el != end_el ; el++ )
+	{
+		const Elem * elem = *el;
+    for (uint nnid=0; nnid < elem->n_nodes(); nnid++)
+		{
+      const Node * nptr = elem->node_ptr(nnid);
+      uint nid = nptr->id();
+
+			// Fills a user-provided std::vector with the boundary ids associated with Node nptr
+      std::vector<boundary_id_type> vec;
+      boundary_info.boundary_ids( nptr, vec );
+			if ( ! vec.size() ) continue;
+
+			Point p = *nptr;
+			libMesh::out << p(0) << ", " << p(1) << ", " << p(2) << std::endl;
+			// Inserção na malha CGAL
+			//CPoint cpt = tf->transf( CPoint( p(0), p(1) ) );
+			//cdt.insert( cpt );
+
+		}
+	}
+	*/
 
   mesh.write("teste.mesh");
 
