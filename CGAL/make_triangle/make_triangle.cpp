36861e79941dfed3af7875528e21809b117f03cf
Luciano Siqueira <lcnsqr@gmail.com>
2020-12-26 09:24:19 -0300

    typedef Point

--- CGAL/make_triangle/make_triangle.cpp
+++ CGAL/make_triangle/make_triangle.cpp
@@ -2,14 +2,14 @@
 #include <CGAL/Polyhedron_3.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel  Kernel;
-typedef Kernel::Point_3                                      Point_3;
+typedef Kernel::Point_3                                      Point;
 typedef CGAL::Polyhedron_3<Kernel>                           Polyhedron;
 
 int main(int argc, char* argv[])
 {
   Polyhedron P;
 
-  P.make_triangle(Point_3(-1,1,1), Point_3(0,0,1), Point_3(-1,-1,1));
+  P.make_triangle(Point(-1,1,1), Point(0,0,1), Point(-1,-1,1));
 
   return EXIT_SUCCESS;
 }
